2f54b17c2a085156517db5acc25e100de52aac49
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-01 22:48:25 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,34 +1,9 @@
 class Book
+  attr_accesor :author, :page_count, :genre
+  attr_reader :title
+
   def initialize(title)
     @title = title
-  end
-
-  def title
-    @title
-  end
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(a)
-    @genre = a
-  end
-
-  def genre
-    @genre
   end
 
   def turn_page
